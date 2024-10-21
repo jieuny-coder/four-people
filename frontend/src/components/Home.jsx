438116becf8d1aa3a5c52815f27b2bf6e9283505
jieuny-coder <cherish08@kakao.com>
2024-10-21 17:50:52 +0900

    김지은 frontend1

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className="screen-container">
+            <img className="screen-replace" src="/images/mobile.png" alt="iPhone Frame" />
+            {/* 추가 UI 요소들을 이곳에 배치하면 됩니다 */}
+            <div className='content'>
+                <p>안녕하소</p>
+            </div>
+        </div>
     )
 }
 
