c85936d09b56c81b0b9864b97d6b040111398d3e
mingoo <country1207@naver.com>
2024-10-25 12:37:27 +0900

    민근 10월 25일 12:37

--- frontend/src/pages/Parkinglot01.jsx
+++ frontend/src/pages/Parkinglot01.jsx
@@ -1,3 +1,4 @@
+import Btnbar from '../components/Btnbar';
 import Topbar from '../components/Topbar';
 import '../index.css';
 
@@ -10,47 +11,25 @@ export const Parkinglot01 = () => {
                 <div className="component-2-child"></div>
                 <div className="vector-3-parent" id="frameContainer">
                     <img className="vector-3-icon" alt="" src="vector-3.svg" />
-                    <div className="div1">주차장 상태</div>
                 </div>
             </div>
-            <div><Topbar/></div>
-            <div className="bottom-bar">
-                <div className="child"></div>
-                <div className="home-indicator"></div>
-                <div className="parent">
-                    <div className="div4">
-                        <img className="sf-symbol-house" alt="" src="SF Symbol/house.svg" />
-                        <div className="home">HOME</div>
-                    </div>
-                    <div className="div5">
-                        <div className="label">주차장 찾기</div>
-                        <img className="search-icon" alt="" src="search.svg" />
-                    </div>
-                    <div className="div6">
-                        <img className="sf-symbol-gearshapefill" alt="" src="SF Symbol/gearshape.fill.svg" />
-                        <div className="setting">설정</div>
-                    </div>
-                    <div className="div7">
-                        <div className="div8">즐겨찾기</div>
-                        <img className="grade-icon" alt="" src="grade.svg" />
-                    </div>
-                </div>
+            <div><Topbar/>
+                <div className="div1">주차장 상태</div>
             </div>
+            <div><Btnbar/></div>
             <div className="div9">
                 <div className="item"></div>
             </div>
-            <div className="or">주차장 상태 사진 or 이미지</div>
             <div className="rectangle-parent">
                 <div className="instance-child"></div>
-                <div className="index">
-                    <div className="div10">1</div>
-                </div>
                 <div className="wrapper">
                     <div className="div11">주차장 주소 나타나는 부분</div>
+                    <img className="icon" alt="" src="images/stars.png" />
                 </div>
-                <img className="icon" alt="" src="별.svg" />
             </div>
-            <div className="inner"></div>
+            <div className="inner">
+            <div className="or">주차장 상태 사진 or 이미지</div>
+            </div>
             <div className="rectangle-group">
                 <div className="group-child"></div>
                 <div className="div12">주차장 정보</div>
---
+++ frontend/src/pages/Parkinglot03.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Parkinglot03 = () => {
+  return (
+    <div>Parkinglot03</div>
+  )
+}
