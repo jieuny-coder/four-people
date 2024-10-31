a21cecabaff72d19980f6fb921597fec108ac66d
mingoo <country1207@naver.com>
2024-11-01 04:10:51 +0900

    민근 11월 01일 04:10

--- frontend/src/pages/ParkingSearch .jsx
+++ frontend/src/pages/ParkingSearch .jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{ useState } from 'react';
 import Btnbar from '../components/Btnbar';
 import '../index.css';
 import BannerBox from '../components/BannerBox';
