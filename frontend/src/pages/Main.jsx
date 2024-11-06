29cfd137473d5981bfe965c12bee37ca232b377c
jieuny-coder <cherish08@kakao.com>
2024-11-06 18:23:01 +0900

    김지은 css+ main

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -4,16 +4,16 @@ import { useNavigate } from 'react-router-dom';
 const Main = () => {
     const navigate = useNavigate();
 
-    const handleClick = () => {
-        console.log("메인페이지 클릭");
+    const handleLogoClick  = () => {
+        console.log("로고 클릭");
         navigate('/login'); // '/login' 경로로 이동
 
     };
 
     return (
-        <div className='main_background' onClick={handleClick}>
+        <div className='main_background'>
             <img src='/images/start_IMG.png' alt='background_image' className='background_image' />
-            <div className='centered_logo_container'>
+            <div className='centered_logo_container' onClick={handleLogoClick}>
                 <img src='/images/logo.jpg' alt='centered_logo_image' className='centered_logo_image' />
             </div>
         </div>
