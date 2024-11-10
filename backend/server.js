204db28c7126fa778ccca629bd75ede8fc0c88f6
mingoo <country1207@naver.com>
2024-11-10 23:52:24 +0900

    민근 11/10 11:52

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const user = require('./Router/user');
 const main = require('./Router/main');
-const parkingad = require('./Router/parkingad');
-const violation = require('./Router/violation')
+const violation = require('./Router/violation');
 const cors = require('cors');
 
 // CORS 미들웨어를 먼저 설정
 app.use(cors({
     origin: 'http://localhost:3000',
-    methods: 'GET, POST',
     credentials: true,  // 쿠키와 세션 정보를 포함시키도록 설정
 }));
 
@@ -23,21 +21,22 @@ app.use(session({
     resave : false,    // 세션을 항상 재 저장하겠냐? 
     secret : "secret",  // 암호화 할때 사용하는 키값
     store : new fileStore(),   // 세션을 저장하기 위한 저장소 셋팅
-    saveUninitialized : false,  //세션에 저장할 내용이 없더라도 저장 여부!
-    secure: false, 
-}));
+    saveUninitialized : false  //세션에 저장할 내용이 없더라도 저장 여부!
+}))
 
 // 라우터 설정
 app.use('/user', user);
 app.use('/', main);
-app.use('/ParkingSearch', parkingad);
-app.use('./ViolationsList',violation);
+// app.use('/ParkingSearch', parkingad);
+app.use('/violation',violation);
+
+
+
 
 app.set('port', process.env.PORT || 4000)
 app.listen(app.get('port'), ()=>{
     console.log(`Server is running on ${app.get('port')}`);
 });
-
 
 
 
